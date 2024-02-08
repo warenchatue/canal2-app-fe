0a088a75a5c468e6e112d44005ba487ce8d35b13
jordan2204 <anafackjordan@gmail.com>
2024-02-08 19:50:10 +0100

    feat: update route rules

--- .app/config/routes-rules.ts
+++ .app/config/routes-rules.ts
@@ -3,7 +3,7 @@ import { isProduction } from 'std-env'
 const staticAssetsRule = isProduction
   ? {
       headers: {
-        'Cache-Control': 'public, max-age=0',
+        'Cache-Control': 'public, max-age=31536000',
       },
     }
   : {}
@@ -18,7 +18,7 @@ const staticPageRule = isProduction
       },
       headers: {
         'Cache-Control':
-          'public, max-age=0, s-maxage=0, stale-while-revalidate=3600, stale-if-error=3600',
+          'public, max-age=3600, s-maxage=3600, stale-while-revalidate=3600, stale-if-error=3600',
       },
     }
   : {}
