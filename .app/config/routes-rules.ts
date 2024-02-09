8fc76efe531a1cc1320db7c491c86b5a60bedcde
jordan2204 <anafackjordan@gmail.com>
2024-02-09 02:38:21 +0100

    feat: update route rules

--- .app/config/routes-rules.ts
+++ .app/config/routes-rules.ts
@@ -37,36 +37,30 @@ const dynamicPageRule = isProduction
     }
   : {}
 
-// const dynamicPageRule = isProduction
-//   ? {
-//       cache: {
-//         swr: false,
-//         ssr: false,
-//         maxAge: 3600,
-//         staleMaxAge: 3600,
-//       },
-//       headers: {
-//         'Cache-Control':
-//           'public, max-age=3600, s-maxage=3600, stale-while-revalidate=3600, stale-if-error=3600',
-//       },
-//     }
-//   : {}
+// export const appRules = {
+//   '/**': { isr: 60 },
+//   '/img/**': staticAssetsRule,
+//   '/shiki/**': staticAssetsRule,
+//   '/api/**': dynamicAPIRule,
+//   '/dashboards': dynamicPageRule,
+//   '/bo/pub': dynamicPageRule,
+//   '/bo/pub/**': dynamicPageRule,
+//   '/bo/sales': dynamicPageRule,
+//   '/bo/sales/**': dynamicPageRule,
+//   '/bo/accountancy': dynamicPageRule,
+//   '/bo/accountancy/**': dynamicPageRule,
+//   '/bo/config/**': dynamicPageRule,
+//   '/bo/admin/**': dynamicPageRule,
+//   '/bo/profile/**': dynamicPageRule,
+//   '/auth/**': dynamicPageRule,
+// }
 
 export const appRules = {
-  '/**': { isr: 60 },
   '/img/**': staticAssetsRule,
   '/shiki/**': staticAssetsRule,
   '/api/**': dynamicAPIRule,
   '/dashboards': dynamicPageRule,
-  '/bo/pub': dynamicPageRule,
-  '/bo/pub/**': dynamicPageRule,
-  '/bo/sales': dynamicPageRule,
-  '/bo/sales/**': dynamicPageRule,
-  '/bo/accountancy/': dynamicPageRule,
-  '/bo/accountancy/**': dynamicPageRule,
-  '/bo/config/**': dynamicPageRule,
-  '/bo/admin/**': dynamicPageRule,
-  '/bo/profile/**': dynamicPageRule,
+  '/bo/**': dynamicPageRule,
   '/auth/**': dynamicPageRule,
 }
 
