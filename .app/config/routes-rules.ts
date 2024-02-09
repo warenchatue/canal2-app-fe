842aa3033efe39a32a378fd1df34be7b02e04ae5
jordan2204 <anafackjordan@gmail.com>
2024-02-09 01:27:02 +0100

    feat: update route rules

--- .app/config/routes-rules.ts
+++ .app/config/routes-rules.ts
@@ -2,6 +2,7 @@ import { isProduction } from 'std-env'
 
 const staticAssetsRule = isProduction
   ? {
+      isr: true,
       headers: {
         'Cache-Control': 'public, max-age=31536000',
       },
@@ -22,18 +23,24 @@ const dynamicAPIRule = isProduction
 
 const dynamicPageRule = isProduction
   ? {
-      cache: {
-        swr: false,
-        ssr: false,
-        maxAge: 3600,
-        staleMaxAge: 3600,
-      },
-      headers: {
-        'Cache-Control':
-          'public, max-age=3600, s-maxage=3600, stale-while-revalidate=3600, stale-if-error=3600',
-      },
+      swr: true,
     }
   : {}
+
+// const dynamicPageRule = isProduction
+//   ? {
+//       cache: {
+//         swr: false,
+//         ssr: false,
+//         maxAge: 3600,
+//         staleMaxAge: 3600,
+//       },
+//       headers: {
+//         'Cache-Control':
+//           'public, max-age=3600, s-maxage=3600, stale-while-revalidate=3600, stale-if-error=3600',
+//       },
+//     }
+//   : {}
 
 export const appRules = {
   '/**': { isr: 60 },
