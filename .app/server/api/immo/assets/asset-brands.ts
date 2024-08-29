65c5a58a428350ceb77b5924fa1b8a460347e7c4
jordan2204 <anafackjordan@gmail.com>
2024-08-29 03:40:29 +0100

    feat: added asset features

---
+++ .app/server/api/immo/assets/asset-brands.ts
@@ -0,0 +1,133 @@
+export default defineEventHandler(async (event) => {
+  const query = getQuery(event)
+  const perPage = parseInt((query.perPage as string) || '5', 10)
+  const page = parseInt((query.page as string) || '1', 10)
+  const filter = (query.filter as string) || ''
+  const action = (query.action as string) || 'get'
+  const id = (query.id as string) || ''
+  const token = (query.token as string) || ''
+
+  if (action == 'findOne') {
+    const data = await findOne(id, token)
+    return { data: data, success: true }
+  } else if (action == 'findAll') {
+    const data = await findAll(token)
+    return {
+      total: data.length,
+      data: filterData(data, filter, page, perPage),
+    }
+  } else if (action == 'createAssetBrand') {
+    const body = await readBody(event)
+    console.log(body)
+    const data = await createAssetBrand(body, token)
+    return { data: data, success: true }
+  } else if (action == 'updateAssetBrand') {
+    const body = await readBody(event)
+    const data = await updateAssetBrand(id, body, token)
+    return { data: data, success: true }
+  } else if (action == 'delete') {
+    const data = await deleteAssetBrand(id, token)
+    return { data: data, success: true }
+  }
+})
+
+function filterData(
+  data: any[],
+  filter: string,
+  page: number,
+  perPage: number,
+) {
+  const offset = (page - 1) * perPage
+  if (!filter) {
+    return data.slice(offset, offset + perPage)
+  }
+  const filterRe = new RegExp(filter, 'i')
+  return data
+    .filter((item) => {
+      return [item.name, item.email, item.status].some((item) =>
+        item.match(filterRe),
+      )
+    })
+    .slice(offset, offset + perPage)
+}
+
+async function findOne(id: string, token: string) {
+  console.log('findOne ' + token)
+  const runtimeConfig = useRuntimeConfig()
+  const data: any = await $fetch(
+    runtimeConfig.env.apiUrl + '/asset-brands/' + id,
+    {
+      method: 'get',
+      headers: {
+        Authorization: 'Bearer ' + token,
+        'Content-type': 'application/json',
+      },
+    },
+  ).catch((error) => console.log(error))
+  console.log(data)
+  return Promise.resolve(data)
+}
+
+async function findAll(token: string) {
+  console.log('findAll ' + token)
+  const runtimeConfig = useRuntimeConfig()
+  const data: any = await $fetch(runtimeConfig.env.apiUrl + '/asset-brands', {
+    method: 'get',
+    headers: {
+      Authorization: 'Bearer ' + token,
+      'Content-type': 'application/json',
+    },
+  }).catch((error) => console.log(error))
+  // console.log(data)
+  return Promise.resolve(data)
+}
+
+async function createAssetBrand(body: any, token: string) {
+  console.log('createAssetBrand ' + token)
+  const runtimeConfig = useRuntimeConfig()
+  const data: any = await $fetch(runtimeConfig.env.apiUrl + '/asset-brands', {
+    method: 'post',
+    headers: {
+      Authorization: 'Bearer ' + token,
+      'Content-type': 'application/json',
+    },
+    body: body,
+  }).catch((error) => console.log(error))
+  console.log(data)
+  return Promise.resolve(data)
+}
+
+async function updateAssetBrand(id: string, body: any, token: string) {
+  console.log('updateAssetBrand ' + token)
+  const runtimeConfig = useRuntimeConfig()
+  const data: any = await $fetch(
+    runtimeConfig.env.apiUrl + '/asset-brands/' + id,
+    {
+      method: 'PUT',
+      headers: {
+        Authorization: 'Bearer ' + token,
+        'Content-type': 'application/json',
+      },
+      body: body,
+    },
+  ).catch((error) => console.log(error))
+  console.log(data)
+  return Promise.resolve(data)
+}
+
+async function deleteAssetBrand(id: string, token: string) {
+  console.log('deleteAssetBrand ' + token)
+  const runtimeConfig = useRuntimeConfig()
+  const data: any = await $fetch(
+    runtimeConfig.env.apiUrl + '/asset-brands/' + id,
+    {
+      method: 'DELETE',
+      headers: {
+        Authorization: 'Bearer ' + token,
+        'Content-type': 'application/json',
+      },
+    },
+  ).catch((error) => console.log(error))
+  console.log(data)
+  return Promise.resolve(data)
+}
---
+++ .app/server/api/immo/assets/asset-categories.ts
@@ -0,0 +1,139 @@
+export default defineEventHandler(async (event) => {
+  const query = getQuery(event)
+  const perPage = parseInt((query.perPage as string) || '5', 10)
+  const page = parseInt((query.page as string) || '1', 10)
+  const filter = (query.filter as string) || ''
+  const action = (query.action as string) || 'get'
+  const id = (query.id as string) || ''
+  const token = (query.token as string) || ''
+
+  if (action == 'findOne') {
+    const data = await findOne(id, token)
+    return { data: data, success: true }
+  } else if (action == 'findAll') {
+    const data = await findAll(token)
+    return {
+      total: data.length,
+      data: filterData(data, filter, page, perPage),
+    }
+  } else if (action == 'createAssetCategory') {
+    const body = await readBody(event)
+    console.log(body)
+    const data = await createAssetCategory(body, token)
+    return { data: data, success: true }
+  } else if (action == 'updateAssetCategory') {
+    const body = await readBody(event)
+    const data = await updateAssetCategory(id, body, token)
+    return { data: data, success: true }
+  } else if (action == 'delete') {
+    const data = await deleteAssetCategory(id, token)
+    return { data: data, success: true }
+  }
+})
+
+function filterData(
+  data: any[],
+  filter: string,
+  page: number,
+  perPage: number,
+) {
+  const offset = (page - 1) * perPage
+  if (!filter) {
+    return data.slice(offset, offset + perPage)
+  }
+  const filterRe = new RegExp(filter, 'i')
+  return data
+    .filter((item) => {
+      return [item.name, item.email, item.status].some((item) =>
+        item.match(filterRe),
+      )
+    })
+    .slice(offset, offset + perPage)
+}
+
+async function findOne(id: string, token: string) {
+  console.log('findOne ' + token)
+  const runtimeConfig = useRuntimeConfig()
+  const data: any = await $fetch(
+    runtimeConfig.env.apiUrl + '/asset-categories/' + id,
+    {
+      method: 'get',
+      headers: {
+        Authorization: 'Bearer ' + token,
+        'Content-type': 'application/json',
+      },
+    },
+  ).catch((error) => console.log(error))
+  console.log(data)
+  return Promise.resolve(data)
+}
+
+async function findAll(token: string) {
+  console.log('findAll ' + token)
+  const runtimeConfig = useRuntimeConfig()
+  const data: any = await $fetch(
+    runtimeConfig.env.apiUrl + '/asset-categories',
+    {
+      method: 'get',
+      headers: {
+        Authorization: 'Bearer ' + token,
+        'Content-type': 'application/json',
+      },
+    },
+  ).catch((error) => console.log(error))
+  // console.log(data)
+  return Promise.resolve(data)
+}
+
+async function createAssetCategory(body: any, token: string) {
+  console.log('createAssetCategory ' + token)
+  const runtimeConfig = useRuntimeConfig()
+  const data: any = await $fetch(
+    runtimeConfig.env.apiUrl + '/asset-categories',
+    {
+      method: 'post',
+      headers: {
+        Authorization: 'Bearer ' + token,
+        'Content-type': 'application/json',
+      },
+      body: body,
+    },
+  ).catch((error) => console.log(error))
+  console.log(data)
+  return Promise.resolve(data)
+}
+
+async function updateAssetCategory(id: string, body: any, token: string) {
+  console.log('updateAssetCategory ' + token)
+  const runtimeConfig = useRuntimeConfig()
+  const data: any = await $fetch(
+    runtimeConfig.env.apiUrl + '/asset-categories/' + id,
+    {
+      method: 'PUT',
+      headers: {
+        Authorization: 'Bearer ' + token,
+        'Content-type': 'application/json',
+      },
+      body: body,
+    },
+  ).catch((error) => console.log(error))
+  console.log(data)
+  return Promise.resolve(data)
+}
+
+async function deleteAssetCategory(id: string, token: string) {
+  console.log('deleteAssetCategory ' + token)
+  const runtimeConfig = useRuntimeConfig()
+  const data: any = await $fetch(
+    runtimeConfig.env.apiUrl + '/asset-categories/' + id,
+    {
+      method: 'DELETE',
+      headers: {
+        Authorization: 'Bearer ' + token,
+        'Content-type': 'application/json',
+      },
+    },
+  ).catch((error) => console.log(error))
+  console.log(data)
+  return Promise.resolve(data)
+}
