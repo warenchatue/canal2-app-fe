251631d456c515842d9133c40c11279a96a75610
jordan2204 <anafackjordan@gmail.com>
2025-03-06 09:32:05 +0100

    feat: update role

--- .app/pages/bo/sales/orders/[type]-[value]-[id].vue
+++ .app/pages/bo/sales/orders/[type]-[value]-[id].vue
@@ -47,6 +47,7 @@ const dates = ref({
 if (
   authStore.user.appRole?.name != UserRole.sale &&
   authStore.user.appRole?.name != UserRole.adminSale &&
+  authStore.user.appRole?.name != UserRole.mediaPlanner &&
   authStore.user.appRole?.name != UserRole.billing &&
   authStore.user.appRole?.name != UserRole.admin &&
   authStore.user.appRole?.name != UserRole.accountancy &&
